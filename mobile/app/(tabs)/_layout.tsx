8f76cf11de433616a2c4a2ae0b60177a1a0978cf
Nathan Imogo <100687502+NathanCavallier@users.noreply.github.com>
2025-05-12 10:30:24 +0200

    feat: Add library and search pages with styling
    - Implemented the library page with a grid layout for books.
    - Created a search page with a search bar and dynamic book filtering.
    - Added global styles and specific styles for library and search components.
    - Introduced new story and privacy policy pages as placeholders.
    - Established a new API endpoint for updating stories.
    - Configured Tailwind CSS for styling across the application.
    - Updated TypeScript configuration for better module resolution and type checking.

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,4 +1,5 @@
-import { Tabs } from 'expo-router';
+import { Tabs, Stack } from 'expo-router';
+import { View } from 'react-native';
 import React from 'react';
 import { Platform } from 'react-native';
 
@@ -12,34 +13,36 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
+    <>
+      <Stack.Screen
+          screenOptions={{
+            tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+            headerShown: false,
+            tabBarButton: HapticTab,
+            tabBarBackground: TabBarBackground,
+            tabBarStyle: Platform.select({
+              ios: {
+                // Use a transparent background on iOS to show the blur effect
+                position: 'absolute',
+              },
+              default: {},
+            }),
+          }}>
+          <Tabs.Screen
+            name="index"
+            options={{
+              title: 'Home',
+              tabBarIcon: ({ color }: { color: string }) => <IconSymbol size={28} name="house.fill" color={color} />,
+            }}
+          />
+          <Tabs.Screen
+            name="explore"
+            options={{
+              title: 'Explore',
+              tabBarIcon: ({ color }: {color: string }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+            }}
+          />
+       </Stack.Screen>
+      </>
   );
 }
